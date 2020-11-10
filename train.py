b74251253ff85c96a8f104675c6a631c370db5bb
gakkii <35442235+gakkiri@users.noreply.github.com>
2020-11-10 13:01:35 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -77,9 +77,9 @@ def train(cfg):
     dl = torch.utils.data.DataLoader(ds, batch_size=cfg.BATCH_SIZE, shuffle=True,
                                      collate_fn=ds.collate_fn, num_workers=cfg.NUM_WORKERS)
 
-    # val_ds = WSGISDDataset(root='datasets/wgisd', resize=cfg.RESIZE, mode='test')
-    # val_dl = torch.utils.data.DataLoader(val_ds, batch_size=1, shuffle=True,
-    #                                      collate_fn=ds.collate_fn, num_workers=4)
+    val_ds = WSGISDDataset(root='datasets/wgisd', resize=cfg.RESIZE, mode='test')
+    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=1, shuffle=True,
+                                         collate_fn=ds.collate_fn, num_workers=4)
 
     model = CascadeRCNN(cfg).cuda()
     optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.lr)
@@ -92,9 +92,9 @@ def train(cfg):
         info = f'[RESULT]: Train. Epoch: {epoch:02d}, summary_loss: {summary_loss.avg:.3f}, time: {(time.time() - t):.3f}, '
         print(info)
 
-        # summary_loss = val_one_epoch(model, val_dl)
-        # info = f'[EVAL]: Train. Epoch: {epoch:02d}, summary_loss: {summary_loss.avg:.3f}, time: {(time.time() - t):.3f}, '
-        # print(info)
+        summary_loss = val_one_epoch(model, val_dl)
+        info = f'[EVAL]: Train. Epoch: {epoch:02d}, summary_loss: {summary_loss.avg:.3f}, time: {(time.time() - t):.3f}, '
+        print(info)
         
         scheduler.step(summary_loss.avg)
         if summary_loss.avg < best_summary_loss:
